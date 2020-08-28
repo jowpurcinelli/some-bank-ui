996893c96dcd968fcb7fde9d2aa628205cee35c9
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-08-28 20:27:07 -0300

    app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//import { ThemeProvider } from 'styled-components';
+//import { AnimatePresence } from 'framer-motion';
+
+//import theme from './styles/theme';
+//import GlobalStyles from './styles/global';
+//import Dashboard from './pages/Dashboard';
+
+const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <AnimatePresence exitBeforeEnter>
+      <Dashboard />
+    </AnimatePresence>
+    <GlobalStyles />
+  </ThemeProvider>
+);
+
+export default App;
